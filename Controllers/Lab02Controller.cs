271cbbc2d92e3ca71b6a605fe76641f470fbb8ec
SV <SV@PM21-PC25>
2026-09-23 10:19:58 +0700

    Add project files.

---
+++ Controllers/Lab02Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2_25dh190025_nguyenhuynhhongquan.Controllers
+{
+    public class Lab02Controller : Controller
+    {
+        // GET: Lab02
+        public ActionResult vd1()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lab2_25dh190025_nguyenhuynhhongquan.MvcApplication" Language="C#" %>
